05adfd3d665a656f87cb46a264e66b7a687f47d1
Tarcísio Thallys <tarcisio_thallys@hotmail.com>
2017-09-05 13:46:32 -0300

    Projeto Agencia Criativa

---
+++ 1-agencia-criativa/app/scripts/codes.js
@@ -0,0 +1,9 @@
+$(function () {
+
+	//$('.carousel').carousel('pause');
+	// $('.galeria-projetos figure .pelicula').mouseleave(function () {
+	// 	$(this).parent().find('.pelicula').fadeOut();
+	// });
+
+
+});
