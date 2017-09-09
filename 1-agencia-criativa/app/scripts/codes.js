b1c2599211a07cd95a72554e6d2510eb40dc6896
tarcisiothallys <katarci2>
2017-09-09 00:54:12 -0300

    Bug de largura corrigido e rodapé finalizado.

--- 1-agencia-criativa/app/scripts/codes.js
+++ 1-agencia-criativa/app/scripts/codes.js
@@ -1,9 +1,14 @@
-$(function () {
-
-	//$('.carousel').carousel('pause');
-	// $('.galeria-projetos figure .pelicula').mouseleave(function () {
-	// 	$(this).parent().find('.pelicula').fadeOut();
-	// });
 
 
-});
+function meuMapa() {
+	var map = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 15,
+      center: {lat: -23.952296, lng: -46.331355},
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        mapTypeIds: ['roadmap', 'terrain']
+      }
+	});
+
+} 
