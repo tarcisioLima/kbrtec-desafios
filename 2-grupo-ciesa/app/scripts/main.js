42b4224407e47adf6e74fd107c2ab1231273bb1b
Tarcísio Thallys <tarcisio_thallys@hotmail.com>
2017-09-22 18:06:11 -0300

    Slider e Serviços concluídos.

--- 2-grupo-ciesa/app/scripts/main.js
+++ 2-grupo-ciesa/app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+
+
+//Items slider
+var totalItems = $('.item').length
+
+$('#main-slider').bind('slid.bs.carousel', function() {
+	
+    let currentIndex =   $('div.active').index() + 1;
+    $('.current').html('0' + currentIndex);
+
+});
+
+$('.controls-slider > div .total').html('/0' + totalItems);
