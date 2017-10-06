bca5395ae5e1595fb796477489e1a78c905a493a
Tarcísio Thallys <tarcisio_thallys@hotmail.com>
2017-10-06 17:58:45 -0300

    Finalizando site.

--- 2-grupo-ciesa/app/scripts/main.js
+++ 2-grupo-ciesa/app/scripts/main.js
@@ -1,33 +1,30 @@
-
 //Scroll To top
 $('#go-to-top').click(function(){
 	$('html,body').animate({ scrollTop: 0 }, 'slow');
 });
 
-
 //Items slider
 var totalItems = $('#main-slider .item').length;
 
-$('#main-slider').bind('slid.bs.carousel', function() {
-	
+$('#main-slider').bind('slid.bs.carousel', function() {	
     let currentIndex =   $('div.active').index() + 1;
     $('.current').html('0' + currentIndex);
-
 });
 
 $('.controls-slider > div .total').html('/0' + totalItems);
 
-// Menu
-// $('.navegacao-expansivel').click(function(){
-// 	$(this).fadeOut();
-	
-// })
-
 $('.navegacao .navegacao-principal .alternador').click(function(){
-	// $('.overlay').show();
-	$('.navegacao-expansivel').show();
-	$('.navegacao-lista').css({transition: "1s", transform: "translateX(0%)"});
 
+	$('.overlay').fadeIn();
+	$('.side-nav').fadeIn().css('transform', 'translateX(0%)');
+	// $('.navegacao-lista').css({transition: "1s", transform: "translateX(0%)"});
+
+});
+
+$('#get-back').click(function(){
+	$('.overlay').fadeOut();
+	$('.navegacao .side-nav').css('transform', 'translateX(100%)');
+	
 });
 
 
