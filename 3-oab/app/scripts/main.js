dbbdbb49f0b040e1b7aee7e97fe39e4f963bcad8
Tarcísio Thallys <tarcisio_thallys@hotmail.com>
2017-11-01 18:44:29 -0200

    Sessão de serviços.

--- 3-oab/app/scripts/main.js
+++ 3-oab/app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	//Slider
+	var swiper = new Swiper('.swiper-container', {
+	  navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      slidesPerView: 4,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {        
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        }
+      }
+    });
+});
