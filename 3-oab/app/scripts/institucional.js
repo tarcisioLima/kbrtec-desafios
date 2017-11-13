d50caa2ebe9e4a4ff0c45d14f29fe933078ef93f
Tarcísio Thallys <tarcisio_thallys@hotmail.com>
2017-11-13 17:06:48 -0200

    Página Institucional.

---
+++ 3-oab/app/scripts/institucional.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$('.quick-menu ul li').click(function(){
+		$('.quick-menu ul li').removeClass('active');
+		$(this).addClass('active');
+		$('.quick-content .item-article').removeClass('active');
+		$(".quick-content").find("[data-item='" + $(this).data('item') + "']").addClass('active');	
+	});
+});
