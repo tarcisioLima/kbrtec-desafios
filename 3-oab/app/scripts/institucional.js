98cbc9e3497aacb85162e311f8f13deed8e26fbd
Tarcísio Thallys <tarcisio_thallys@hotmail.com>
2017-11-14 18:17:30 -0200

    Página de eventos.

--- 3-oab/app/scripts/institucional.js
+++ 3-oab/app/scripts/institucional.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 		$('.quick-menu ul li').removeClass('active');
 		$(this).addClass('active');
 		$('.quick-content .item-article').removeClass('active');
-		$(".quick-content").find("[data-item='" + $(this).data('item') + "']").addClass('active');	
+		$(".quick-content").find("[data-item='" + $(this).data('item') + "']").addClass('active').delay(1000);	
 	});
 });
