cd2236d1dd9c029c83628ecd75f4a4b6953bf63f
Tarcísio Thallys <tarcisio_thallys@hotmail.com>
2017-11-23 18:17:06 -0200

    Cadastro currículo.

--- 3-oab/app/scripts/cadastro.js
+++ 3-oab/app/scripts/cadastro.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 	console.log('foiii');
+	$('#vai').click(function(e){
+		e.preventDefault();
+		$('#inputfile').click();
+	});
+	$('#inputfile').change(function(){
+		var nome = "n mudou";
+	    if($('#inputfile').val().length > 0){
+	    	nome = $(this).val();
+	    } 
+	    console.log(nome);
+	    alert(nome);
+	});
 });
